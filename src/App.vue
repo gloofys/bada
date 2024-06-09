6e03c9fa664a9d12eef726d657fef1e1d8b9861e
fredb <fred.brosman06@gmail.com>
2024-06-09 20:15:39 +0300

    css improvements

--- src/App.vue
+++ src/App.vue
@@ -18,18 +18,4 @@ export default {
 }
 </script>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
 
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-}
-</style>
