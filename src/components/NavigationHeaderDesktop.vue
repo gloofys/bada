a563db924c82fd4e82781328893e6fc12070a71b
fredb <fred.brosman06@gmail.com>
2024-06-10 20:36:34 +0300

    added css icons, and menu button, fixed dropdowns css issues

--- src/components/NavigationHeaderDesktop.vue
+++ src/components/NavigationHeaderDesktop.vue
@@ -1,55 +1,97 @@
 <template>
   <header class="navigation-header">
-    <nav>
-      <ul>
-        <li><router-link to="/">{{ $t('home.home') }}</router-link></li>
-        <li class="dropdown">
-          <span>{{ $t('housing.housing') }}</span>
-          <ul class="dropdown-content">
-            <li><router-link to="/housing/rooms">{{ $t('housing.rooms') }}</router-link></li>
-            <li><router-link to="/housing/pricing">{{ $t('housing.pricing') }}</router-link></li>
-            <li><router-link to="/housing/extra-services">{{ $t('housing.extra_services') }}</router-link></li>
-            <li><router-link to="/housing/booking-policies">{{ $t('housing.booking_policies') }}</router-link></li>
-          </ul>
-        </li>
-        <li class="dropdown">
-          <span>{{ $t('wineRegion.wine_region') }}</span>
-          <ul class="dropdown-content">
-            <li><router-link to="/wineRegion/biggest-wine-producers">{{ $t('wineRegion.biggest_wine_producers') }}</router-link></li>
-            <li><router-link to="/wineRegion/small-wine-producers">{{ $t('wineRegion.small_wine_producers') }}</router-link></li>
-            <li><router-link to="/wineRegion/our-favourites">{{ $t('wineRegion.our_favourites') }}</router-link></li>
-          </ul>
-        </li>
-        <li class="dropdown">
-          <span>{{ $t('location.location') }}</span>
-          <ul class="dropdown-content">
-            <li><router-link to="/location/how-to-get-here">{{ $t('location.how_to_get_here') }}</router-link></li>
-          </ul>
-        </li>
-        <li class="dropdown">
-          <span>{{ $t('activities.activities') }}</span>
-          <ul class="dropdown-content">
-            <li><router-link to="/activities/hiking">{{ $t('activities.hiking') }}</router-link></li>
-            <li><router-link to="/activities/cycling">{{ $t('activities.cycling') }}</router-link></li>
-            <li><router-link to="/activities/lakeBalaton">{{ $t('activities.lakeBalaton') }}</router-link></li>
-            <li><router-link to="/activities/beaches">{{ $t('activities.beaches') }}</router-link></li>
-            <li><router-link to="/activities/nearby-cities">{{ $t('activities.nearby_cities') }}</router-link></li>
-            <li><router-link to="/activities/spa">{{ $t('activities.spa') }}</router-link></li>
-            <li><router-link to="/activities/badacsonyEvents">{{ $t('activities.badacsonyEvents') }}</router-link></li>
-          </ul>
-        </li>
-        <li><router-link to="/gallery">{{ $t('gallery') }}</router-link></li>
-        <li><router-link to="/contact">{{ $t('contact') }}</router-link></li>
-      </ul>
-    </nav>
-    <div class="navigation-content">
+    <div class="nav-container">
+      <nav>
+        <ul>
+          <li>
+            <router-link to="/">{{ $t('home.home') }}</router-link>
+          </li>
+          <li class="dropdown">
+            <span>{{ $t('housing.housing') }}</span>
+            <ul class="dropdown-content">
+              <li>
+                <router-link to="/housing/rooms">{{ $t('housing.rooms') }}</router-link>
+              </li>
+              <li>
+                <router-link to="/housing/pricing">{{ $t('housing.pricing') }}</router-link>
+              </li>
+              <li>
+                <router-link to="/housing/extra-services">{{ $t('housing.extra_services') }}</router-link>
+              </li>
+              <li>
+                <router-link to="/housing/booking-policies">{{ $t('housing.booking_policies') }}</router-link>
+              </li>
+            </ul>
+          </li>
+          <li class="dropdown">
+            <span>{{ $t('wineRegion.wine_region') }}</span>
+            <ul class="dropdown-content">
+              <li>
+                <router-link to="/wineRegion/biggest-wine-producers">{{
+                    $t('wineRegion.biggest_wine_producers')
+                  }}
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/wineRegion/small-wine-producers">{{
+                    $t('wineRegion.small_wine_producers')
+                  }}
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/wineRegion/our-favourites">{{ $t('wineRegion.our_favourites') }}</router-link>
+              </li>
+            </ul>
+          </li>
+          <li class="dropdown">
+            <span>{{ $t('location.location') }}</span>
+            <ul class="dropdown-content">
+              <li>
+                <router-link to="/location/how-to-get-here">{{ $t('location.how_to_get_here') }}</router-link>
+              </li>
+            </ul>
+          </li>
+          <li class="dropdown">
+            <span>{{ $t('activities.activities') }}</span>
+            <ul class="dropdown-content">
+              <li>
+                <router-link to="/activities/hiking">{{ $t('activities.hiking') }}</router-link>
+              </li>
+              <li>
+                <router-link to="/activities/cycling">{{ $t('activities.cycling') }}</router-link>
+              </li>
+              <li>
+                <router-link to="/activities/lakeBalaton">{{ $t('activities.lakeBalaton') }}</router-link>
+              </li>
+              <li>
+                <router-link to="/activities/beaches">{{ $t('activities.beaches') }}</router-link>
+              </li>
+              <li>
+                <router-link to="/activities/nearby-cities">{{ $t('activities.nearby_cities') }}</router-link>
+              </li>
+              <li>
+                <router-link to="/activities/spa">{{ $t('activities.spa') }}</router-link>
+              </li>
+              <li>
+                <router-link to="/activities/badacsonyEvents">{{ $t('activities.badacsonyEvents') }}</router-link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <router-link to="/gallery">{{ $t('gallery') }}</router-link>
+          </li>
+          <li>
+            <router-link to="/contact">{{ $t('contact') }}</router-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
       <div class="language-selector">
         <select @change="changeLanguage($event.target.value)">
           <option value="en">🇬🇧 English</option>
           <option value="et">🇪🇪 Eesti</option>
         </select>
       </div>
-    </div>
   </header>
 </template>
 
--- src/components/NavigationHeaderMobile.vue
+++ src/components/NavigationHeaderMobile.vue
@@ -1,18 +1,32 @@
 <template>
   <header class="navigation-header-mobile">
-    <button @click="toggleDropdown" class="dropdown-button">
-      Menu
-    </button>
+    <div class="menu-icon-container">
+      <div class="box-menu-icon" @click="toggleDropdown">
+        <div class="menu-icon"></div>
+        <div class="menu-icon"></div>
+        <div class="menu-icon"></div>
+      </div>
+    </div>
     <ul v-if="showDropdown" class="dropdown-content">
-      <li @click="closeDropdown"><router-link to="/">{{ $t('home.home') }}</router-link></li>
+      <li @click="closeDropdown">
+        <router-link to="/">{{ $t('home.home') }}</router-link>
+      </li>
       <li>
         <details>
           <summary>{{ $t('housing.housing') }}</summary>
           <ul class="dropdown-subcontent">
-            <li @click="closeDropdown"><router-link to="/housing/rooms">{{ $t('housing.rooms') }}</router-link></li>
-            <li @click="closeDropdown"><router-link to="/housing/pricing">{{ $t('housing.pricing') }}</router-link></li>
-            <li @click="closeDropdown"><router-link to="/housing/extra-services">{{ $t('housing.extra_services') }}</router-link></li>
-            <li @click="closeDropdown"><router-link to="/housing/booking-policies">{{ $t('housing.booking_policies') }}</router-link></li>
+            <li @click="closeDropdown">
+              <router-link to="/housing/rooms">{{ $t('housing.rooms') }}</router-link>
+            </li>
+            <li @click="closeDropdown">
+              <router-link to="/housing/pricing">{{ $t('housing.pricing') }}</router-link>
+            </li>
+            <li @click="closeDropdown">
+              <router-link to="/housing/extra-services">{{ $t('housing.extra_services') }}</router-link>
+            </li>
+            <li @click="closeDropdown">
+              <router-link to="/housing/booking-policies">{{ $t('housing.booking_policies') }}</router-link>
+            </li>
           </ul>
         </details>
       </li>
@@ -20,9 +34,21 @@
         <details>
           <summary>{{ $t('wineRegion.wine_region') }}</summary>
           <ul class="dropdown-subcontent">
-            <li @click="closeDropdown"><router-link to="/wineRegion/biggest-wine-producers">{{ $t('wineRegion.biggest_wine_producers') }}</router-link></li>
-            <li @click="closeDropdown"><router-link to="/wineRegion/small-wine-producers">{{ $t('wineRegion.small_wine_producers') }}</router-link></li>
-            <li @click="closeDropdown"><router-link to="/wineRegion/our-favourites">{{ $t('wineRegion.our_favourites') }}</router-link></li>
+            <li @click="closeDropdown">
+              <router-link to="/wineRegion/biggest-wine-producers">{{
+                  $t('wineRegion.biggest_wine_producers')
+                }}
+              </router-link>
+            </li>
+            <li @click="closeDropdown">
+              <router-link to="/wineRegion/small-wine-producers">{{
+                  $t('wineRegion.small_wine_producers')
+                }}
+              </router-link>
+            </li>
+            <li @click="closeDropdown">
+              <router-link to="/wineRegion/our-favourites">{{ $t('wineRegion.our_favourites') }}</router-link>
+            </li>
           </ul>
         </details>
       </li>
@@ -30,7 +56,9 @@
         <details>
           <summary>{{ $t('location.location') }}</summary>
           <ul class="dropdown-subcontent">
-            <li @click="closeDropdown"><router-link to="/location/how-to-get-here">{{ $t('location.how_to_get_here') }}</router-link></li>
+            <li @click="closeDropdown">
+              <router-link to="/location/how-to-get-here">{{ $t('location.how_to_get_here') }}</router-link>
+            </li>
           </ul>
         </details>
       </li>
@@ -38,27 +66,39 @@
         <details>
           <summary>{{ $t('activities.activities') }}</summary>
           <ul class="dropdown-subcontent">
-            <li @click="closeDropdown"><router-link to="/activities/hiking">{{ $t('activities.hiking') }}</router-link></li>
-            <li @click="closeDropdown"><router-link to="/activities/cycling">{{ $t('activities.cycling') }}</router-link></li>
-            <li @click="closeDropdown"><router-link to="/activities/lakeBalaton">{{ $t('activities.lakeBalaton') }}</router-link></li>
-            <li @click="closeDropdown"><router-link to="/activities/beaches">{{ $t('activities.beaches') }}</router-link></li>
-            <li @click="closeDropdown"><router-link to="/activities/nearby-cities">{{ $t('activities.nearby_cities') }}</router-link></li>
-            <li @click="closeDropdown"><router-link to="/activities/spa">{{ $t('activities.spa') }}</router-link></li>
-            <li @click="closeDropdown"><router-link to="/activities/badacsonyEvents">{{ $t('activities.badacsonyEvents') }}</router-link></li>
+            <li @click="closeDropdown">
+              <router-link to="/activities/hiking">{{ $t('activities.hiking') }}</router-link>
+            </li>
+            <li @click="closeDropdown">
+              <router-link to="/activities/cycling">{{ $t('activities.cycling') }}</router-link>
+            </li>
+            <li @click="closeDropdown">
+              <router-link to="/activities/lakeBalaton">{{ $t('activities.lakeBalaton') }}</router-link>
+            </li>
+            <li @click="closeDropdown">
+              <router-link to="/activities/beaches">{{ $t('activities.beaches') }}</router-link>
+            </li>
+            <li @click="closeDropdown">
+              <router-link to="/activities/nearby-cities">{{ $t('activities.nearby_cities') }}</router-link>
+            </li>
+            <li @click="closeDropdown">
+              <router-link to="/activities/spa">{{ $t('activities.spa') }}</router-link>
+            </li>
+            <li @click="closeDropdown">
+              <router-link to="/activities/badacsonyEvents">{{ $t('activities.badacsonyEvents') }}</router-link>
+            </li>
           </ul>
         </details>
       </li>
-      <li @click="closeDropdown"><router-link to="/gallery">{{ $t('gallery') }}</router-link></li>
-      <li @click="closeDropdown"><router-link to="/contact">{{ $t('contact') }}</router-link></li>
+      <li @click="closeDropdown">
+        <router-link to="/gallery">{{ $t('gallery') }}</router-link>
+      </li>
+      <li @click="closeDropdown">
+        <router-link to="/contact">{{ $t('contact') }}</router-link>
+      </li>
+            <li @click="changeLanguage('en')">🇬🇧 English</li>
+            <li @click="changeLanguage('et')">🇪🇪 Eesti</li>
     </ul>
-    <div class="navigation-content">
-      <div class="language-selector">
-        <select @change="changeLanguage($event.target.value)">
-          <option value="en">🇬🇧 English</option>
-          <option value="et">🇪🇪 Eesti</option>
-        </select>
-      </div>
-    </div>
   </header>
 </template>
 
@@ -85,11 +125,6 @@ export default {
 </script>
 
 <style scoped>
-
-
-
-
-
 
 
 </style>
