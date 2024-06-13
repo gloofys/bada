2465f64c224981269d982e6ca8eab9ce50fe5242
fredb <fred.brosman06@gmail.com>
2024-06-13 15:34:02 +0300

    added hiking trail image. added text and image to hikingview. updated translations.

--- src/views/Hiking.vue
+++ src/views/Hiking.vue
@@ -26,6 +26,32 @@
       <p>{{ $t('hikingExperiences.description') }}</p>
     </div>
   </div>
+<div class="container">
+  <p>{{ $t('hikingExperiences.main_introduction') }}</p>
+  <h1>The badacsony hill trail</h1>
+  <img
+      src="/hiking_images/badahikingtrail_1500.jpg"
+      srcset="
+        /hiking_images/badahikingtrail_375.jpg 375w,
+        /hiking_images/badahikingtrail_550.jpg 550w,
+        /hiking_images/badahikingtrail_750.jpg 750w,
+        /hiking_images/badahikingtrail_1100.jpg 1100w,
+        /hiking_images/badahikingtrail_1500.jpg 1500w,
+        /hiking_images/badahikingtrail_2000.jpg 2000w,
+        /hiking_images/badahikingtrail_3000.jpg 3000w
+      "
+      sizes="(max-width: 375px) 375w,
+             (max-width: 550px) 550w,
+             (max-width: 750px) 750w,
+             (max-width: 1100px) 1100w,
+             (max-width: 1500px) 1500w,
+             (max-width: 2000px) 2000w,
+             100vw"
+      alt="Badacsony hiking trail pictures"
+      class="banner-image"
+  />
+  <p>{{ $t('hikingExperiences.main_content') }}</p>
+</div>
 </template>
 
 
