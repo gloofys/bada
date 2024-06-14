94990cbd1b890e908b45cc698030b2b3512826e8
fredb <fred.brosman06@gmail.com>
2024-06-14 12:09:01 +0300

    added desktop and mobile view css. made css changes to hikingview. added a hikingview picture, but commented it out atm.

--- src/views/Hiking.vue
+++ src/views/Hiking.vue
@@ -26,32 +26,64 @@
       <p>{{ $t('hikingExperiences.description') }}</p>
     </div>
   </div>
-<div class="container">
-  <p>{{ $t('hikingExperiences.main_introduction') }}</p>
-  <h1>The badacsony hill trail</h1>
-  <img
-      src="/hiking_images/badahikingtrail_1500.jpg"
-      srcset="
-        /hiking_images/badahikingtrail_375.jpg 375w,
-        /hiking_images/badahikingtrail_550.jpg 550w,
-        /hiking_images/badahikingtrail_750.jpg 750w,
-        /hiking_images/badahikingtrail_1100.jpg 1100w,
-        /hiking_images/badahikingtrail_1500.jpg 1500w,
-        /hiking_images/badahikingtrail_2000.jpg 2000w,
-        /hiking_images/badahikingtrail_3000.jpg 3000w
-      "
-      sizes="(max-width: 375px) 375w,
-             (max-width: 550px) 550w,
-             (max-width: 750px) 750w,
-             (max-width: 1100px) 1100w,
-             (max-width: 1500px) 1500w,
-             (max-width: 2000px) 2000w,
-             100vw"
-      alt="Badacsony hiking trail pictures"
-      class="banner-image"
-  />
-  <p>{{ $t('hikingExperiences.main_content') }}</p>
-</div>
+  <div class="container">
+    <p>{{ $t('hikingExperiences.main_introduction') }}</p>
+
+    <!-- Wrapper for the first text and image pair -->
+    <div class="text-image-wrapper">
+      <div class="text-content">
+        <h3>The Badacsony Hill Trail</h3>
+        <p>{{ $t('hikingExperiences.main_content') }}</p>
+      </div>
+      <img
+          src="/hiking_images/badahikingtrail_1500.jpg"
+          srcset="
+          /hiking_images/badahikingtrail_375.jpg 375w,
+          /hiking_images/badahikingtrail_550.jpg 550w,
+          /hiking_images/badahikingtrail_750.jpg 750w,
+          /hiking_images/badahikingtrail_1100.jpg 1100w,
+          /hiking_images/badahikingtrail_1500.jpg 1500w,
+          /hiking_images/badahikingtrail_2000.jpg 2000w,
+          /hiking_images/badahikingtrail_3000.jpg 3000w
+        "
+          sizes="(max-width: 375px) 375w,
+               (max-width: 550px) 550w,
+               (max-width: 750px) 750w,
+               (max-width: 1100px) 1100w,
+               (max-width: 1500px) 1500w,
+               (max-width: 2000px) 2000w,
+               100vw"
+          alt="Badacsony hiking trail pictures"
+          class="content-image"
+      />
+    </div>
+<!--    <div class="text-image-wrapper">-->
+<!--      <div class="text-content">-->
+<!--        <h4>View from top of the hill</h4>-->
+<!--      </div>-->
+<!--      <img-->
+<!--          src="/hiking_images/badahikingview_1500.jpg"-->
+<!--          srcset="-->
+<!--          /hiking_images/badahikingview_375.jpg 375w,-->
+<!--          /hiking_images/badahikingview_550.jpg 550w,-->
+<!--          /hiking_images/badahikingview_750.jpg 750w,-->
+<!--          /hiking_images/badahikingview_1100.jpg 1100w,-->
+<!--          /hiking_images/badahikingview_1500.jpg 1500w,-->
+<!--          /hiking_images/badahikingview_2000.jpg 2000w,-->
+<!--          /hiking_images/badahikingview_3000.jpg 3000w-->
+<!--        "-->
+<!--          sizes="(max-width: 375px) 375w,-->
+<!--               (max-width: 550px) 550w,-->
+<!--               (max-width: 750px) 750w,-->
+<!--               (max-width: 1100px) 1100w,-->
+<!--               (max-width: 1500px) 1500w,-->
+<!--               (max-width: 2000px) 2000w,-->
+<!--               100vw"-->
+<!--          alt="Badacsony hiking trail pictures"-->
+<!--          class="content-image"-->
+<!--      />-->
+<!--    </div>-->
+  </div>
 </template>
 
 
