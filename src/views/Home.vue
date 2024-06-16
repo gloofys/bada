7fd88f5f2c5d7fcea0474f797e53be65e61dd9ab
fredb <fred.brosman06@gmail.com>
2024-06-17 00:17:55 +0300

    started rooms layout.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,3 +9,6 @@ export default {
   name: 'Home'
 }
 </script>
+
+
+magick ./public/rooms_images/1st_layout_transparent_png.png -resize 550x ./public/rooms_images/1st_layout_transparent_png_1500.png
--- src/views/Rooms.vue
+++ src/views/Rooms.vue
@@ -1,16 +1,45 @@
 <template>
-  <div>
-    <h1>Rooms Page</h1>
+  <div class="floor-plan">
+    <img
+        src="/rooms_images/1st_layout_transparent_png.png"
+        srcset="
+        /rooms_images/1st_layout_transparent_png_550.png 550w,
+        /rooms_images/1st_layout_transparent_png_1100.png 1100w,
+        /rooms_images/1st_layout_transparent_png_1500.png 1500w
+      "
+        sizes="
+        (max-width: 550px) 550px,
+        (max-width: 1100px) 1100px,
+        (max-width: 1500px) 1500px,
+        100vw
+      "
+        alt="Floor Plan"
+        class="content-image"
+    />
+    <!-- Hotspots for rooms -->
+    <div class="hotspot kitchen" data-room="Kitchen"></div>
+    <div class="hotspot kitchen1" data-room="Kitchen"></div>
+    <div class="hotspot wine-cellar" data-room="Wine Cellar"></div>
+    <div class="hotspot bedroom" data-room="Bedroom"></div>
+    <div class="hotspot bedroom" data-room="Bedroom"></div>
+    <div class="hotspot bedroom" data-room="Bedroom"></div>
+    <div class="hotspot bedroom" data-room="Bedroom"></div>
+    <div class="hotspot bedroom" data-room="Bedroom"></div>
+    <div class="hotspot bedroom" data-room="Bedroom"></div>
+
+    <!-- Display hovered room images -->
+    <div class="hover-image kitchen">
+      <img src="/rooms_images/1st_example.jpg" alt="Kitchen Image" class="room-image" />
+    </div>
+    <div class="hover-image wine-cellar">
+      <img src="/rooms_images/2nd_example.jpg" alt="Wine Cellar Image" class="room-image" />
+    </div>
+    <div class="hover-image bedroom">
+      <img src="/rooms_images/2nd_example.jpg" alt="Bedroom Image" class="room-image" />
+    </div>
   </div>
 </template>
 
-
 <script>
-export default {
-  name: 'Rooms'
-}
+
 </script>
-
-<style scoped>
-
-</style>
