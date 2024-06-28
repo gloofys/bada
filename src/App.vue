1e8bf4ff918dd98b494faf2108cc5843c35d3e54
fredb <fred.brosman06@gmail.com>
2024-06-28 22:05:23 +0300

    added footer.vue component and footer.css. also et and en.json. css needs more work

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,18 @@
     <main>
       <router-view />
     </main>
+    <Footer />
   </div>
 </template>
 
 <script>
 import NavigationHeader from './components/NavigationHeader.vue';
+import Footer from "@/components/Footer.vue";
 
 export default {
   name: 'App',
   components: {
+    Footer,
     NavigationHeader
   }
 }
---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="site-footer">
+    <div class="footer-content">
+      <div class="company-info">
+        <h3>{{ $t('footer.companyInfoTitle') }}</h3>
+        <p>{{ $t('footer.companyName') }}</p>
+        <p>{{ $t('footer.companyAddress') }}</p>
+      </div>
+      <div class="contact-info">
+        <h3>{{ $t('footer.contactInfoTitle') }}</h3>
+        <p>{{ $t('footer.phone') }}</p>
+        <p>{{ $t('footer.email') }}</p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+};
+</script>
+
+
