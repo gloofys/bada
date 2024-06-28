67b9e12adcb0502ccbd9eade90a95f892d27925b
fredb <fred.brosman06@gmail.com>
2024-06-28 22:42:23 +0300

    footer improvements

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,12 +2,10 @@
   <footer class="site-footer">
     <div class="footer-content">
       <div class="company-info">
-        <h3>{{ $t('footer.companyInfoTitle') }}</h3>
         <p>{{ $t('footer.companyName') }}</p>
         <p>{{ $t('footer.companyAddress') }}</p>
       </div>
       <div class="contact-info">
-        <h3>{{ $t('footer.contactInfoTitle') }}</h3>
         <p>{{ $t('footer.phone') }}</p>
         <p>{{ $t('footer.email') }}</p>
       </div>
