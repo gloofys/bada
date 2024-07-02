9ba7a09ccb43c2bfe408792f621c8bec8f166af9
fredb <fred.brosman06@gmail.com>
2024-07-02 20:04:50 +0300

    completed ExtraServices.vue and BookingPolicies.vue.

--- src/views/BookingPolicies.vue
+++ src/views/BookingPolicies.vue
@@ -1,16 +1,55 @@
 <template>
-  <div>
-    <h1>Booking policies Page</h1>
+  <div class="container">
+    <section class="booking-policies">
+      <h3>{{ $t('bookingPolicies.reservationPolicy') }}</h3>
+      <ul>
+        <li>{{ $t('bookingPolicies.reservationPolicyText1') }}</li>
+        <li>{{ $t('bookingPolicies.reservationPolicyText2') }}</li>
+        <li>{{ $t('bookingPolicies.reservationPolicyText3') }}</li>
+      </ul>
+
+      <h3>{{ $t('bookingPolicies.cancellationPolicy') }}</h3>
+      <ul>
+        <li>{{ $t('bookingPolicies.cancellationPolicyText1') }}</li>
+        <li>{{ $t('bookingPolicies.cancellationPolicyText2') }}</li>
+        <li>{{ $t('bookingPolicies.cancellationPolicyText3') }}</li>
+      </ul>
+
+      <h3>{{ $t('bookingPolicies.noShowPolicy') }}</h3>
+      <ul>
+        <li>{{ $t('bookingPolicies.noShowPolicyText') }}</li>
+      </ul>
+      <h3>{{ $t('bookingPolicies.additionalServices') }}</h3>
+      <ul>
+        <li>{{ $t('bookingPolicies.additionalServicesText1') }}</li>
+        <li>{{ $t('bookingPolicies.additionalServicesText2') }}</li>
+        <li>{{ $t('bookingPolicies.additionalServicesText3') }}</li>
+      </ul>
+
+      <h3>{{ $t('bookingPolicies.childrenPolicy') }}</h3>
+      <ul>
+        <li>{{ $t('bookingPolicies.childrenPolicyText1') }}</li>
+        <li>{{ $t('bookingPolicies.childrenPolicyText2') }}</li>
+      </ul>
+
+      <h3>{{ $t('bookingPolicies.petsPolicy') }}</h3>
+      <ul>
+        <li>{{ $t('bookingPolicies.petsPolicyText1') }}</li>
+        <li>{{ $t('bookingPolicies.petsPolicyText2') }}</li>
+      </ul>
+
+      <h3>{{ $t('bookingPolicies.damagePolicy') }}</h3>
+      <ul>
+        <li>{{ $t('bookingPolicies.damagePolicyText1') }}</li>
+        <li>{{ $t('bookingPolicies.damagePolicyText2') }}</li>
+      </ul>
+    </section>
   </div>
 </template>
 
-
 <script>
 export default {
-  name: 'BookingPolicies'
+  name: 'BookingPolicies',
+
 }
 </script>
-
-<style scoped>
-
-</style>
--- src/views/ExtraServices.vue
+++ src/views/ExtraServices.vue
@@ -1,6 +1,41 @@
 <template>
-  <div>
-    <h1>Extra services Page</h1>
+  <div class="container">
+    <!-- Introduction Section -->
+    <section class="intro-section">
+      <h2>{{ $t('extraServices.heading') }}</h2>
+      <p>{{ $t('extraServices.intro') }}</p>
+    </section>
+
+    <!-- Section 1: Room Prices -->
+    <section class="pricing-section">
+      <table>
+        <tr>
+          <th>{{ $t('extraServices.service') }}</th>
+          <th>{{ $t('extraServices.price') }} <span class="note">{{ $t('extraServices.perPerson') }}</span></th>
+          <th>{{ $t('extraServices.description') }}</th>
+        </tr>
+        <tr>
+          <td>{{ $t('extraServices.breakfast') }}</td>
+          <td>7€</td>
+          <td>{{ $t('extraServices.breakfastDetails') }}</td>
+        </tr>
+        <tr>
+          <td>{{ $t('extraServices.wineTasting') }}</td>
+          <td>7€</td>
+          <td>{{ $t('extraServices.wineTastingDetails') }}</td>
+        </tr>
+        <tr>
+          <td>{{ $t('extraServices.transport') }}</td>
+          <td>{{ $t('extraServices.transportPrice') }}</td>
+          <td>{{ $t('extraServices.transportDetails') }}</td>
+        </tr>
+      </table>
+    </section>
+    <section class="booking-info">
+      <p>{{ $t('extraServices.message') }}</p>
+      <a @click="goToContacts">{{ $t('bookingInfo.contactUs') }}</a>
+    </section>
+
   </div>
 </template>
 
