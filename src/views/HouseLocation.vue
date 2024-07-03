769c935040d13440b4ac413b05365d9844bad88a
fredb <fred.brosman06@gmail.com>
2024-07-03 19:52:32 +0300

    house location WIP

--- src/views/HouseLocation.vue
+++ src/views/HouseLocation.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container-maps">
 
     <h3>Római út 245, Badacsonytomaj,  Hungary, 8261</h3>
 
@@ -10,15 +10,15 @@
     <!-- Mobile Map -->
     <iframe class="mobile-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d682.9244452534077!2d17.480548269668784!3d46.790559216405946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476900c0026887a1%3A0x9eb4dfa4c3fffd99!2sBadacsonytomaj%2C%20R%C3%B3mai%20%C3%BAt%20245%2C%208261%20Ungari!5e0!3m2!1set!2sfi!4v1720014732489!5m2!1set!2sfi" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
-    <section class="house-location">
-    <ul>
-      <li><strong>Closest Airport:</strong> Budapest Ferenc Liszt International Airport (BUD) - approximately 200 km</li>
-      <li><strong>Closest Train Station:</strong> Badacsony Train Station - 2.5 km</li>
-      <li><strong>Distance to Budapest:</strong> 180 km</li>
-      <li><strong>Distance to Vienna:</strong> 210 km</li>
-      <li><strong>Distance to Tallinn:</strong> 1,900 km</li>
-    </ul>
-    </section>
+      <div class="house-location">
+        <ul>
+          <li><strong>Closest Airport:</strong> Budapest Ferenc Liszt International Airport (BUD) - approximately 200 km</li>
+          <li><strong>Closest Train Station:</strong> Badacsony Train Station - 2.5 km</li>
+          <li><strong>Distance to Budapest:</strong> 180 km</li>
+          <li><strong>Distance to Vienna:</strong> 210 km</li>
+          <li><strong>Distance to Tallinn:</strong> 1,900 km</li>
+        </ul>
+      </div>
   </div>
 </template>
 
