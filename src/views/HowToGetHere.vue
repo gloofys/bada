850af1ae2ffe6b58865693c71d7f119a16e355cd
fredb <fred.brosman06@gmail.com>
2024-07-03 21:24:15 +0300

    HowToGetHere.vue started and WIP

--- src/views/HowToGetHere.vue
+++ src/views/HowToGetHere.vue
@@ -1,6 +1,37 @@
 <template>
-  <div>
-    <h1>How to get here Page</h1>
+  <div class="container">
+    <h2 class="page-heading">How to Get Here</h2>
+
+    <section class="transportation-option">
+      <h3>By Car</h3>
+      <p>Driving to our house is a great option if you enjoy scenic routes and the flexibility of having your own vehicle. Here are the directions:</p>
+      <ul>
+        <li>From Budapest: Take the M7 motorway towards Balaton, then follow the signs to Badacsony.</li>
+        <li>From Vienna: Take the A2 motorway towards Graz, then follow the signs to Hungary and continue towards Badacsony.</li>
+        <li>Parking is available on-site.</li>
+      </ul>
+    </section>
+
+    <section class="transportation-option">
+      <h3>By Plane and Rental Car</h3>
+      <p>If you prefer to fly, you can rent a car at the airport. Here are the details:</p>
+      <ul>
+        <li>Fly to Budapest Ferenc Liszt International Airport (BUD) or Vienna International Airport.</li>
+        <li>Rent a car at the airport from one of the major car rental companies.</li>
+        <li>Drive to our house following the directions provided in the 'By Car' section.</li>
+      </ul>
+    </section>
+
+    <section class="transportation-option">
+      <h3>By Plane and Train</h3>
+      <p>For those who prefer not to drive, you can take a train after your flight. Here are the steps:</p>
+      <ul>
+        <li>Fly to Budapest Ferenc Liszt International Airport (BUD).</li>
+        <li>Take a taxi or public transport to Budapest Déli Railway Station.</li>
+        <li>Take a train to Badacsony Train Station.</li>
+        <li>From the train station, take a taxi to our house, which is approximately 2.5 km away.</li>
+      </ul>
+    </section>
   </div>
 </template>
 
