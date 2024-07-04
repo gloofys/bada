808ff9f773b99413e356c72849c28988f3e38cab
fredb <fred.brosman06@gmail.com>
2024-07-04 17:38:25 +0300

    translations for HowToGetHere.vue

--- src/views/HowToGetHere.vue
+++ src/views/HowToGetHere.vue
@@ -1,35 +1,35 @@
 <template>
   <div class="container">
-    <h2 class="page-heading">How to Get Here</h2>
+    <h2 class="page-heading">{{ $t('howToGetHere.title') }}</h2>
 
     <section class="transportation-option">
-      <h3>By Car</h3>
-      <p>Driving to our house is a great option if you enjoy scenic routes and the flexibility of having your own vehicle. Here are the directions:</p>
+      <h3>{{ $t('howToGetHere.byCar.title') }}</h3>
+      <p>{{ $t('howToGetHere.byCar.description') }}</p>
       <ul>
-        <li>From Budapest: Take the M7 motorway towards Balaton, then follow the signs to Badacsony.</li>
-        <li>From Vienna: Take the A2 motorway towards Graz, then follow the signs to Hungary and continue towards Badacsony.</li>
-        <li>Parking is available on-site.</li>
+        <li>{{ $t('howToGetHere.byCar.direction1') }}</li>
+        <li>{{ $t('howToGetHere.byCar.direction2') }}</li>
+        <li>{{ $t('howToGetHere.byCar.direction3') }}</li>
       </ul>
     </section>
 
     <section class="transportation-option">
-      <h3>By Plane and Rental Car</h3>
-      <p>If you prefer to fly, you can rent a car at the airport. Here are the details:</p>
+      <h3>{{ $t('howToGetHere.byPlaneRentalCar.title') }}</h3>
+      <p>{{ $t('howToGetHere.byPlaneRentalCar.description') }}</p>
       <ul>
-        <li>Fly to Budapest Ferenc Liszt International Airport (BUD) or Vienna International Airport.</li>
-        <li>Rent a car at the airport from one of the major car rental companies.</li>
-        <li>Drive to our house following the directions provided in the 'By Car' section.</li>
+        <li>{{ $t('howToGetHere.byPlaneRentalCar.detail1') }}</li>
+        <li>{{ $t('howToGetHere.byPlaneRentalCar.detail2') }}</li>
+        <li>{{ $t('howToGetHere.byPlaneRentalCar.detail3') }}</li>
       </ul>
     </section>
 
     <section class="transportation-option">
-      <h3>By Plane and Train</h3>
-      <p>For those who prefer not to drive, you can take a train after your flight. Here are the steps:</p>
+      <h3>{{ $t('howToGetHere.byPlaneTrain.title') }}</h3>
+      <p>{{ $t('howToGetHere.byPlaneTrain.description') }}</p>
       <ul>
-        <li>Fly to Budapest Ferenc Liszt International Airport (BUD).</li>
-        <li>Take a taxi or public transport to Budapest Déli Railway Station.</li>
-        <li>Take a train to Badacsony Train Station.</li>
-        <li>From the train station, take a taxi to our house, which is approximately 2.5 km away.</li>
+        <li>{{ $t('howToGetHere.byPlaneTrain.step1') }}</li>
+        <li>{{ $t('howToGetHere.byPlaneTrain.step2') }}</li>
+        <li>{{ $t('howToGetHere.byPlaneTrain.step3') }}</li>
+        <li>{{ $t('howToGetHere.byPlaneTrain.step4') }}</li>
       </ul>
     </section>
   </div>
