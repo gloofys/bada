6fb2db56e9a438cfb6c15247ca129f8cc13b8898
fredb <fred.brosman06@gmail.com>
2024-07-09 20:55:42 +0300

    progress on LakeBalaton.vue

--- src/views/LakeBalaton.vue
+++ src/views/LakeBalaton.vue
@@ -1,7 +1,33 @@
 <template>
-  <div>
-    <h1>Balaton Page</h1>
-  </div>
+    <div class="container">
+      <!-- Introduction Section -->
+      <section class="intro-section-lake">
+        <h2>{{ $t('lakeBalaton.introTitle') }}</h2>
+        <p>{{ $t('lakeBalaton.introText1') }}</p>
+        <p>{{ $t('lakeBalaton.introText2') }}</p>
+      </section>
+
+      <!-- Activities Section -->
+      <section class="activities-section">
+        <!-- Swimming Section -->
+        <div class="activity">
+          <h3>{{ $t('lakeBalaton.swimmingTitle') }}</h3>
+          <p>{{ $t('lakeBalaton.swimmingDescription') }}</p>
+        </div>
+
+        <!-- Sailing Section -->
+        <div class="activity">
+          <h3>{{ $t('lakeBalaton.sailingTitle') }}</h3>
+          <p>{{ $t('lakeBalaton.sailingDescription') }}</p>
+        </div>
+
+        <!-- Water Skiing & Wakeboarding Section -->
+        <div class="activity">
+          <h3>{{ $t('lakeBalaton.waterSkiingTitle') }}</h3>
+          <p>{{ $t('lakeBalaton.waterSkiingDescription') }}</p>
+        </div>
+      </section>
+    </div>
 </template>
 
 
