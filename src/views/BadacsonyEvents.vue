97674f1112d9c16661b3c78042d03176c5025e4c
fredb <fred.brosman06@gmail.com>
2024-07-10 20:37:10 +0300

    finished events page. css improvements

--- src/views/BadacsonyEvents.vue
+++ src/views/BadacsonyEvents.vue
@@ -1,16 +1,84 @@
 <template>
-  <div>
-    <h1>Events Page</h1>
+  <div class="container">
+    <div v-for="event in events" :key="event.title" class="events">
+      <h3>{{ event.title }}</h3>
+      <p><strong>{{ event.dates }}</strong></p>
+      <p>{{ event.description }}</p>
+      <p><em>{{ event.location }}</em></p>
+    </div>
+    <div class = booking-info>
+    <a :href="'https://badacsony.com/en/events'" target="_blank">{{ $t('bookingInfo.eventsWebsite') }}</a>
+    </div>
   </div>
 </template>
 
-
 <script>
 export default {
-  name: 'BadacsonyEvents'
+  name: 'BadacsonyEvents',
+  data() {
+    return {
+      events: [
+        {
+          title: this.$t('events.wineWeek.title'),
+          dates: this.$t('events.wineWeek.dates'),
+          description: this.$t('events.wineWeek.description'),
+          location: this.$t('events.wineWeek.location')
+        },
+        {
+          title: this.$t('events.abrahamhegyWineDays.title'),
+          dates: this.$t('events.abrahamhegyWineDays.dates'),
+          description: this.$t('events.abrahamhegyWineDays.description'),
+          location: this.$t('events.abrahamhegyWineDays.location')
+        },
+        {
+          title: this.$t('events.greybeardFestival.title'),
+          dates: this.$t('events.greybeardFestival.dates'),
+          description: this.$t('events.greybeardFestival.description'),
+          location: this.$t('events.greybeardFestival.location')
+        },
+        {
+          title: this.$t('events.kingStStephenDays.title'),
+          dates: this.$t('events.kingStStephenDays.dates'),
+          description: this.$t('events.kingStStephenDays.description'),
+          location: this.$t('events.kingStStephenDays.location')
+        },
+        {
+          title: this.$t('events.medievalDay.title'),
+          dates: this.$t('events.medievalDay.dates'),
+          description: this.$t('events.medievalDay.description'),
+          location: this.$t('events.medievalDay.location')
+        },
+        {
+          title: this.$t('events.harvestFestival.title'),
+          dates: this.$t('events.harvestFestival.dates'),
+          description: this.$t('events.harvestFestival.description'),
+          location: this.$t('events.harvestFestival.location')
+        },
+        {
+          title: this.$t('events.perchFestival.title'),
+          dates: this.$t('events.perchFestival.dates'),
+          description: this.$t('events.perchFestival.description'),
+          location: this.$t('events.perchFestival.location')
+        },
+        {
+          title: this.$t('events.wineTriathlon.title'),
+          dates: this.$t('events.wineTriathlon.dates'),
+          description: this.$t('events.wineTriathlon.description'),
+          location: this.$t('events.wineTriathlon.location')
+        },
+        {
+          title: this.$t('events.newYearsEveSwim.title'),
+          dates: this.$t('events.newYearsEveSwim.dates'),
+          description: this.$t('events.newYearsEveSwim.description'),
+          location: this.$t('events.newYearsEveSwim.location')
+        }
+      ]
+    }
+  },
+  methods: {
+    redirectToEventsPage() {
+      window.location.href = 'https://badacsony.com/en/events';
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
--- src/views/NearbyCities.vue
+++ src/views/NearbyCities.vue
@@ -1,28 +1,27 @@
 <template>
   <div class="container">
-    <h1>{{ $t('nearbyCitiesPageTitle') }}</h1>
-    <div>
-      <h2>Szigliget</h2>
+    <div class="events">
+      <h3>Szigliget</h3>
       <p><strong>{{ $t('szigliget.distance') }}</strong></p>
       <p>{{ $t('szigliget.description') }}</p>
     </div>
-    <div>
-      <h2>Tapolca</h2>
+    <div class="events">
+      <h3>Tapolca</h3>
       <p><strong>{{ $t('tapolca.distance') }}</strong></p>
       <p>{{ $t('tapolca.description') }}</p>
     </div>
-    <div>
-      <h2>Keszthely</h2>
+    <div class="events">
+      <h3>Keszthely</h3>
       <p><strong>{{ $t('keszthely.distance') }}</strong></p>
       <p>{{ $t('keszthely.description') }}</p>
     </div>
-    <div>
-      <h2>Balatonfüred</h2>
+    <div class="events">
+      <h3>Balatonfüred</h3>
       <p><strong>{{ $t('balatonfured.distance') }}</strong></p>
       <p>{{ $t('balatonfured.description') }}</p>
     </div>
-    <div>
-      <h2>Hévíz</h2>
+    <div class="events">
+      <h3>Hévíz</h3>
       <p><strong>{{ $t('heviz.distance') }}</strong></p>
       <p>{{ $t('heviz.description') }}</p>
     </div>
