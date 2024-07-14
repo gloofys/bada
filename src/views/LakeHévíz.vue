479ded717eefca88ddeefa3496ad07c0bf2c2a4d
fredb <fred.brosman06@gmail.com>
2024-07-14 19:52:40 +0300

    css improvements

--- src/views/LakeHévíz.vue
+++ src/views/LakeHévíz.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <h2>{{ $t('lakeHevizPageTitle') }}</h2>
+    <img src="/nearbycities_images/heviz.png" alt="image of heviz" class="basic-image">
     <p>{{ $t('lakeHeviz.description') }}</p>
   </div>
 </template>
