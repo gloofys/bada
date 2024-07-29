44bdde7010c62e65f32ef567c69b8d8e806fe83b
fredb <fred.brosman06@gmail.com>
2024-07-29 18:49:19 +0300

    test 5 for mobile lightbox zoom using midpoint and removing resetzoom, added limit to max 100% zoom out, pinching allowed whole screen

--- src/components/Lightbox.vue
+++ src/components/Lightbox.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="isOpen" class="lightbox">
+  <div v-if="isOpen" class="lightbox" @touchstart="handleTouchStart" @touchmove="handleTouchMove" @touchend="handleTouchEnd">
     <span class="close" @click="closeLightbox">&times;</span>
     <div class="lightbox-content-container" ref="lightboxContainer">
       <div class="caption">{{ images[currentImageIndex].alt }}</div>
@@ -9,9 +9,6 @@
             :src="images[currentImageIndex].src"
             :alt="images[currentImageIndex].alt"
             ref="lightboxImage"
-            @touchstart="isMobile && handleTouchStart"
-            @touchmove="isMobile && handleTouchMove"
-            @touchend="isMobile && handleTouchEnd"
         />
       </div>
     </div>
@@ -39,11 +36,15 @@ export default {
     return {
       isOpen: true,
       currentImageIndex: this.initialIndex,
-      isMobile: false, // Define isMobile here
+      isMobile: false,
       initialDistance: null,
       initialScale: 1,
       scale: 1,
       lastTouchEnd: 0,
+      lastX: 0,
+      lastY: 0,
+      posX: 0,
+      posY: 0,
     };
   },
   methods: {
@@ -85,26 +86,41 @@ export default {
     },
     resetZoom() {
       this.scale = 1;
-      this.$refs.lightboxImage.style.transform = 'scale(1)';
-      this.$refs.lightboxImage.style.transformOrigin = 'center center';
+      this.posX = 0;
+      this.posY = 0;
+      this.updateTransform();
+    },
+    updateTransform() {
+      this.$refs.lightboxImage.style.transform = `scale(${this.scale}) translate(${this.posX}px, ${this.posY}px)`;
     },
     handleTouchStart(event) {
       console.log('Touch start event:', event);
-      if (this.isMobile && event.touches.length === 2) {
+      if (event.touches.length === 2) {
         this.initialDistance = this.getDistance(event.touches);
         this.initialScale = this.scale;
         this.setTransformOrigin(event.touches);
         console.log('Touch start:', this.initialDistance);
+      } else if (event.touches.length === 1 && this.scale > 1) {
+        this.lastX = event.touches[0].clientX;
+        this.lastY = event.touches[0].clientY;
       }
     },
     handleTouchMove(event) {
       console.log('Touch move event:', event);
-      if (this.isMobile && event.touches.length === 2 && this.initialDistance) {
+      if (event.touches.length === 2 && this.initialDistance) {
         const currentDistance = this.getDistance(event.touches);
         const scaleChange = currentDistance / this.initialDistance;
         this.scale = Math.max(1, this.initialScale * scaleChange); // Ensure scale doesn't go below 1
-        this.$refs.lightboxImage.style.transform = `scale(${this.scale})`;
+        this.updateTransform();
         console.log('Touch move:', currentDistance, this.scale);
+      } else if (event.touches.length === 1 && this.scale > 1) {
+        const deltaX = event.touches[0].clientX - this.lastX;
+        const deltaY = event.touches[0].clientY - this.lastY;
+        this.posX += deltaX;
+        this.posY += deltaY;
+        this.lastX = event.touches[0].clientX;
+        this.lastY = event.touches[0].clientY;
+        this.updateTransform();
       }
     },
     handleTouchEnd(event) {
@@ -135,11 +151,6 @@ export default {
     window.addEventListener('resize', this.checkMobileView);
     this.setupHammer();
 
-    const lightboxImage = this.$refs.lightboxImage;
-    lightboxImage.addEventListener('touchstart', this.handleTouchStart);
-    lightboxImage.addEventListener('touchmove', this.handleTouchMove);
-    lightboxImage.addEventListener('touchend', this.handleTouchEnd);
-
     document.body.style.overflow = 'hidden'; // Prevent scrolling
   },
   beforeDestroy() {
@@ -149,6 +160,8 @@ export default {
   }
 };
 </script>
+
+
 
 <style scoped>
 .lightbox {
