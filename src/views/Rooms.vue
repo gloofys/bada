0c0bfe8a758b1ce4a7ef3a0f7d04c8b75006a47c
fredb <fred.brosman06@gmail.com>
2024-09-01 19:51:05 +0300

    rooms page image update

--- src/views/Rooms.vue
+++ src/views/Rooms.vue
@@ -19,7 +19,7 @@
     </div>
     <div class="room-container reverse-layout">
       <div class="room-content">
-        <h2>Downstairs Living Room</h2>
+        <h2>2nd floor NE bedroom</h2>
         <p><strong>Room size:</strong> 25 m²</p>
         <ul>
           <li>Free Wi-Fi</li>
@@ -31,12 +31,12 @@
         <p><strong>Room fits:</strong> 1x Double Bed + 1x extra bed/Baby COT or 2x Twin + 1x extra bed/Baby COT</p>
       </div>
       <div class="room-image">
-        <img src="/room_images/1st_floor/1st_living_room.jpg" alt="Standard Room Image">
+        <img src="/room_images/2nd_floor/2nd_bedroom_NE.jpg" alt="Standard Room Image">
       </div>
     </div>
     <div class="room-container">
       <div class="room-content">
-        <h2>Downstairs Living Room</h2>
+        <h2>2nd floor SW bedroom</h2>
         <p><strong>Room size:</strong> 25 m²</p>
         <ul>
           <li>Free Wi-Fi</li>
@@ -48,12 +48,12 @@
         <p><strong>Room fits:</strong> 1x Double Bed + 1x extra bed/Baby COT or 2x Twin + 1x extra bed/Baby COT</p>
       </div>
       <div class="room-image">
-        <img src="/room_images/1st_floor/1st_living_room.jpg" alt="Standard Room Image">
+        <img src="/room_images/2nd_floor/2nd_bedroom_SW.jpg" alt="Standard Room Image">
       </div>
     </div>
     <div class="room-container reverse-layout">
       <div class="room-content">
-        <h2>Downstairs Living Room</h2>
+        <h2>2nd floor bedroom with balcony</h2>
         <p><strong>Room size:</strong> 25 m²</p>
         <ul>
           <li>Free Wi-Fi</li>
